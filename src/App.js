a73032120e2f48c4eef4834f94d1f6db8f8d59ca
sachin <sachinnirate24@gmail.com>
2023-09-17 11:52:35 +0530

    kk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React ,{useState} from "react";
+
+import { Navbar } from "./components/Navbar";
+import { Routes } from "./components/Routes";
+import { Footer } from "./components/Footer";
 
 const App = () => {
+
+    const[darkTheme, setDarkTheme] = useState(false);
+
     return(
-        <div>
-            <h1>App</h1>
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className = 'bg-gray-500'>
+                APP
+
+            </div>
         </div>
     );
 }
